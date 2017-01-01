e6383899c634609cc0d8d19f23a5cb73de4165e5
gy <gy@gy-PC>
2017-01-01 16:57:02 +0800

    update js

--- wx-admin/src/main/webapp/project/admin/assets/js/supersized-init.js
+++ wx-admin/src/main/webapp/project/admin/assets/js/supersized-init.js
@@ -3,7 +3,7 @@ jQuery(function($){
     $.supersized({
 
         // Functionality
-        slide_interval     : 4000,    // Length between transitions
+        slide_interval     : 6000,    // Length between transitions
         transition         : 1,    // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
         transition_speed   : 1000,    // Speed of transition
         performance        : 1,    // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
@@ -20,8 +20,8 @@ jQuery(function($){
         // Components
         slide_links        : 'blank',    // Individual links for each slide (Options: false, 'num', 'name', 'blank')
         slides             : [    // Slideshow Images
-                                 //{image : '${imageServerAdmin}/assets/img/backgrounds/1.jpg'},
-                                 //{image : '${imageServerAdmin}/assets/img/backgrounds/2.jpg'},
+                                 {image : '${imageServerAdmin}/assets/img/backgrounds/1.jpg'},
+                                 {image : '${imageServerAdmin}/assets/img/backgrounds/2.jpg'},
                                  {image : '${imageServerAdmin}/assets/img/backgrounds/3.jpg'}
                        ]
 
